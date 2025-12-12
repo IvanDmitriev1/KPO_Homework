ec7f122c43dfdc26a00dc94d2332731e13f0a6af
Ivan Dmitriev <42055372+IvanDmitriev1@users.noreply.github.com>
2025-12-12 15:07:27 +0300

    Configured app host

--- KPO_HW4/KPO_HW4.AppHost/AppHost.cs
+++ KPO_HW4/KPO_HW4.AppHost/AppHost.cs
@@ -1,9 +1,68 @@
+using Aspire.Hosting.Yarp.Transforms;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.KPO_HW4_Api>("api");
+var env = builder.AddDockerComposeEnvironment("docker")
+    .WithProperties(env =>
+    {
+        env.DefaultNetworkName = "my-network";
+    })
+    .WithDashboard(dashboard =>
+    {
+        dashboard.WithHostPort(18888);
+        dashboard.WithForwardedHeaders();
+    });
 
-builder.AddProject<Projects.KPO_HW4_OrderService>("orderService");
+var rabbitmq = builder
+    .AddRabbitMQ("messaging")
+    .WithManagementPlugin()
+    .WithDataVolume()
+    .PublishAsDockerComposeService((resource, service) => {});
 
-builder.AddProject<Projects.KPO_HW4_PaymentsService>("kpo-hw4-paymentsservice");
+var postgres = builder
+    .AddPostgres("postgres")
+    .WithDataVolume()
+    .PublishAsDockerComposeService((resource, service) => { });
+
+var ordersDb = postgres.AddDatabase("orders-db");
+var paymentsDb = postgres.AddDatabase("payments-db");
+
+var paymentsService = builder
+    .AddProject<Projects.KPO_HW4_PaymentsService>("paymentsService")
+    .WithReference(rabbitmq)
+    .WithReference(paymentsDb)
+    .WaitFor(paymentsDb)
+    .WaitFor(rabbitmq)
+    .PublishAsDockerComposeService((resource, service) => { });
+
+var ordersService = builder.AddProject<Projects.KPO_HW4_OrderService>("orderService")
+    .WithReference(rabbitmq)
+    .WithReference(ordersDb)
+    .WaitFor(ordersDb)
+    .WaitFor(rabbitmq)
+    .PublishAsDockerComposeService((resource, service) => { });
+
+var ui = builder.AddProject<Projects.KPO_HW4_Ui>("ui")
+    .PublishAsDockerComposeService((resource, service) => { });
+
+var gateway = builder.AddYarp("gateway")
+    .WithHostPort(8080)
+    .WithConfiguration(yarp =>
+    {
+        yarp.AddRoute("/api/orders/{**catch-all}", ordersService)
+            .WithTransformPathRemovePrefix("/api");
+
+        yarp.AddRoute("/api/payments/{**catch-all}", paymentsService)
+            .WithTransformPathRemovePrefix("/api");
+
+        yarp.AddRoute("/{**catch-all}", ui);
+    })
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Ports.Add("8080:5000");
+    });
+
+ui.WithReference(gateway)
+    .WaitFor(gateway);
 
 builder.Build().Run();
---
+++ KPO_HW4/KPO_HW4.Ui/Program.cs
@@ -0,0 +1,29 @@
+using KPO_HW4.Ui.Components;
+using LumexUI.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddServiceDefaults();
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddLumexServices();
+
+var app = builder.Build();
+app.MapDefaultEndpoints();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
